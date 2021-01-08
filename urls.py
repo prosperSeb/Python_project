f436bd2eae16fed4a405e513e8a78f7228c6cfad
prosperSeb <50331503+prosperSeb@users.noreply.github.com>
2021-01-08 13:45:26 +0100

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^songs/$' , views.song_list ),
-    url(r'^songs/(?P<pk>[0-9]+)/$' , views.song_detail),
-    url(r'^predict/$' , views.predict )
-]
